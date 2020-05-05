90e365e519a07172306297bc4f3459f734487ef7
EricHeee <47197672+EricHeee@users.noreply.github.com>
2020-05-05 13:29:35 -0700

    Update ProjectEuler.py

--- ProjectEuler.py
+++ ProjectEuler.py
@@ -11,4 +11,18 @@ def multiples():
     return li
 
 
-   
+
+# Checks if a number n is a prime or not
+def isPrime(n):
+    i = 2
+    
+    # If n equals 1 or 2 it is definately a prime number
+    if n == 1 or n == 2:
+        return True
+    
+    # Divide n with i to see if n mod i is 0 or not. If not, n is a prime.
+    while (i * i <= n):
+        if n % i == 0:
+            return False
+        i += 1
+    return True
